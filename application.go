9f6be707aee9ef7e15ce1ef76a2c42b3fc9c7448
Guilherme Dantas <guidanoli@proton.me>
2024-03-08 22:53:37 -0300

    refactor: simplify application

--- application.go
+++ application.go
@@ -10,8 +10,7 @@ import (
 )
 
 type CalculatorApp struct {
-	Value   int64
-	OpCount int64
+	Value int64
 }
 
 func (a *CalculatorApp) Advance(
@@ -28,41 +27,22 @@ func (a *CalculatorApp) Advance(
 	if err != nil {
 		return err
 	}
-	switch which := req.Which(); which {
+	switch req.Which() {
 	case AdvanceRequest_Which_add:
 		a.Value += req.Add().Operand()
-		a.OpCount += 1
 	case AdvanceRequest_Which_mul:
 		a.Value *= req.Mul().Operand()
-		a.OpCount += 1
 	case AdvanceRequest_Which_div:
 		a.Value /= req.Div().Operand()
-		a.OpCount += 1
 	default:
-		return fmt.Errorf("unknown advance request: %v", which)
+		return fmt.Errorf("unknown advance request: %v", req)
 	}
 	return nil
 }
 
 func (a *CalculatorApp) Inspect(env rollmelette.EnvInspector, payload []byte) error {
-	msg, err := capnp.Unmarshal(payload)
-	if err != nil {
-		return err
-	}
-	req, err := ReadRootInspectRequest(msg)
-	if err != nil {
-		return err
-	}
-	switch which := req.Which(); which {
-	case InspectRequest_Which_value:
-		msg := fmt.Sprintf("%v", a.Value)
-		env.Report([]byte(msg))
-	case InspectRequest_Which_opCount:
-		msg := fmt.Sprintf("%v", a.OpCount)
-		env.Report([]byte(msg))
-	default:
-		return fmt.Errorf("unknown inspect request: %v", which)
-	}
+	msg := fmt.Sprintf("%v", a.Value)
+	env.Report([]byte(msg))
 	return nil
 }
 
